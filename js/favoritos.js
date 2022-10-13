5f9d2764b2787946756247bcc5ff7b720d84b5b1
Mauricio Teruel <mauricioteruel98@gmail.com>
2022-10-12 22:47:37 -0300

    Funcionalidades de favoritos y publicado agregadas

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,42 +1,9 @@
-const agregarFavoritos = document.getElementById("agregar-favoritos");
-
-agregarFavoritos.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    let datos = {
-        id: document.getElementById("idPelicula"),
-        nombre: document.getElementById("nombrePelicula"),
-        descripcion: document.getElementById("descripcionPelicula"),
-        imagen: document.getElementById("imagenPelicula")
-    }
-
-    let favoritos = localStorage.getItem("favoritos") || [];
-    favoritos = JSON.parse(favoritos);
-
-    let listaPeliculas = favoritos.findIndex((event) => {
-        return event.id === datos.id;
-    })
-    if(listaPeliculas > -1){
-        favoritos.splice(listaPeliculas, 1);
-    }else{
-        favoritos.push(datos);
-    }
-
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-});
-
-
-let favoritos = localStorage.getItem("favoritos") || [];
-favoritos = JSON.stringify(favoritos);
-
-let ul = document.createElement('ul');
-for(let x = 0; x < favoritos.length; x++){
-    let li = document.createElement("li");
-    var a = document.createElement("a");
-    a.href= favoritos[x].nombre;
-    a.textContent = favoritos[x].nombre;
-    li.appendChild(a);
-    ul.appendChild(li);
+//Marcar como favorito
+const marcarFavorito = (id) => {
+    const peliculaFiltrada = peliculas.find((pelicula) => {
+        return pelicula.id === id
+    });
+    peliculaFiltrada.favorito = !peliculaFiltrada.favorito;
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
+    location.reload();
 }
-
-document.getElementById("favoritos").appendChild(ul);
