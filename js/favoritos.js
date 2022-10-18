0fbff1a90c1d7749843786ab8efa0cee5753d943
clementecejas <cuchi_cejas@live.com>
2022-10-18 15:08:35 -0300

    borrando merge de logadmin

--- js/favoritos.js
+++
@@ -1,9 +0,0 @@
-//Marcar como favorito
-const marcarFavorito = (id) => {
-    const peliculaFiltrada = peliculas.find((pelicula) => {
-        return pelicula.id === id
-    });
-    peliculaFiltrada.favorito = !peliculaFiltrada.favorito;
-    localStorage.setItem("peliculas", JSON.stringify(peliculas));
-    location.reload();
-}
