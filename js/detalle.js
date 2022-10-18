1e4146cee646c54091b996fad2ddc679ff80a092
Mauricio Teruel <mauricioteruel98@gmail.com>
2022-10-18 18:45:18 -0300

    Detalle de peliculas

--- js/detalle.js
+++ js/detalle.js
@@ -1,5 +1,35 @@
 let peliculaSeleccionada = JSON.parse(localStorage.getItem("peliculaSeleccionada")) || [];
 
 let detallePelicula = document.getElementById('detallePelicula');
+let tituloPelicula = document.getElementById('tituloPelicula');
+let trailerPelicula = document.getElementById('trailerPelicula');
 
-detallePelicula.innerHTML = ``
+detallePelicula.innerHTML = `
+
+<div class="box1-pag-inf">
+      <img src="${peliculaSeleccionada.imagen}" alt="${peliculaSeleccionada.nombre}">
+    </div>
+    <div class="box2-pag-inf">
+      <div>
+        <h2>${peliculaSeleccionada.nombre}</h2>
+      </div>
+      <div>
+        <p>
+        ${peliculaSeleccionada.descripcion}
+        </p>
+      </div>
+    </div>
+
+`
+
+tituloPelicula.innerHTML = `
+<h1>${peliculaSeleccionada.nombre}</h1>
+`
+
+trailerPelicula.innerHTML = `
+<iframe width="100%" height="315" src="${peliculaSeleccionada.trailer}" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+`
+
